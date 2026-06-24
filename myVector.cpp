993dbe7f67bb5dd57bc4812dba1760b67bdf5616
soways11 <157223054+soways11@users.noreply.github.com>
2026-06-24 17:55:29 +0300

    Update myVector.cpp

--- myVector.cpp
+++ myVector.cpp
@@ -1,13 +1,13 @@
-template <typename type> class myVector{
+template <typename type> class vector{
     private:
         long long int size;
         long long int capacity;
         void prResize(); // set capacity = 2 * size and relocate memory
     public:
         type* elements;
-        myVector(long long int n = 0); // base constructor with set size or stardant size = 0
-        myVector(long long int n, type x); // overloaded constructor with set size and standart element
-        ~myVector(); // destructor: deleting pointer to array
+        vector(long long int n = 0); // base constructor with set size or stardant size = 0
+        vector(long long int n, type x); // overloaded constructor with set size and standart element
+        ~vector(); // destructor: deleting pointer to array
         void resize(long long int n = 0); // the same as base constructor
         void resize(long long int n, type x); // the same as overloaded constructor
         long long int getSize(); // return size
@@ -25,7 +25,7 @@ template <typename type> class myVector{
         long long int count(type x);  // count how many elements array contains
         long long int count(type x, long long int st, long long int fin); // overloaded find with start and finish
 };
-template <typename type> void myVector<type>::prResize(){
+template <typename type> void vector<type>::prResize(){
     type* xelements = elements; // change variable, that contains pointer for array
     elements = new type[size * 2]; // allocate memory for new array
     for (int i = 0; i < size - 1 ; i++){
@@ -34,12 +34,12 @@ template <typename type> void myVector<type>::prResize(){
     delete [] xelements; // deallocate memory of old array
     capacity = 2 * size;
 }
-template <typename type> myVector<type>::myVector(long long int n){
+template <typename type> vector<type>::vector(long long int n){
     elements = new type[n]; // allocate memory
     size = n;
     capacity = n;
 }
-template <typename type> myVector<type>::myVector(long long int n, type x){
+template <typename type> vector<type>::vector(long long int n, type x){
     elements = new type[n]; // allocate memory
     for (int i = 0; i < n; i++){
         elements[i] = x;
@@ -47,50 +47,47 @@ template <typename type> myVector<type>::myVector(long long int n, type x){
     size = n;
     capacity = n;
 }
-template <typename type> myVector<type>::~myVector(){
+template <typename type> vector<type>::~vector(){
     delete [] elements; // deallocate memory of array
 }
-template <typename type> long long int myVector<type>::getSize(){
+template <typename type> long long int vector<type>::getSize(){
     return size;
 }
-template <typename type> long long int myVector<type>::getCapacity(){
+template <typename type> long long int vector<type>::getCapacity(){
     return capacity;
 }
-template <typename type> type* myVector<type>::begin(){
+template <typename type> type* vector<type>::begin(){
     return elements;
 }
-template <typename type> type* myVector<type>::end(){
+template <typename type> type* vector<type>::end(){
     return elements + size;
 }
-template <typename type> void myVector<type>::resize(long long int n){
+template <typename type> void vector<type>::resize(long long int n){
     delete [] elements; // deallocate memory of old array
     elements = new type[n]; // allocate memory for new array
     size = n;
     capacity = n;
 }
-template <typename type> void myVector<type>::resize(long long int n, type x){
-    delete [] elements; // deallocate memory of old array
-    elements = new type[n]; // allocate memory for new array
+template <typename type> void vector<type>::resize(long long int n, type x){
+    resize(n);
     for (int i = 0; i < n; i++){
         elements[i] = x;
     }
-    size = n;
-    capacity = n;
 }
-template <typename type> void myVector<type>::pushBack(type x){
+template <typename type> void vector<type>::pushBack(type x){
     size = size + 1;
     if (size >= capacity){
         prResize(); // resize if array cant save new variable
     }
     elements[size-1] = x;
 }
-template <typename type> void myVector<type>::popBack(){
+template <typename type> void vector<type>::popBack(){
     size = size - 1;
     if (size*4 <= capacity){
         prResize(); // resize if capacity >= 4 * size
     }
 }
-template <typename type> void myVector<type>::pushFront(type x){
+template <typename type> void vector<type>::pushFront(type x){
     size = size + 1;
     if (size >= capacity){
         prResize(); // resize if array can't save new variable
@@ -100,7 +97,7 @@ template <typename type> void myVector<type>::pushFront(type x){
     }
     elements[0] = x;
 }
-template <typename type> void myVector<type>::popFront(){
+template <typename type> void vector<type>::popFront(){
     size = size - 1;
     for (int i = 0; i < size; i++){
         elements[i] = elements[i+1];
@@ -109,7 +106,7 @@ template <typename type> void myVector<type>::popFront(){
         prResize(); // resize if capacity >= 4 * size
     }
 }
-template <typename type> void myVector<type>::insert(type x, long long int pos){
+template <typename type> void vector<type>::insert(type x, long long int pos){
     size = size + 1;
     if (size >= capacity){
         prResize(); // resize if array can't save new variable
@@ -119,7 +116,7 @@ template <typename type> void myVector<type>::insert(type x, long long int pos){
     }
     elements[pos] = x;
 }
-template <typename type> void myVector<type>::erase(long long int pos, long long int count){
+template <typename type> void vector<type>::erase(long long int pos, long long int count){
     size = size - count;
     for (int i = pos; i < pos + count; i++){
         elements[i] = elements[i + count];
@@ -128,7 +125,7 @@ template <typename type> void myVector<type>::erase(long long int pos, long long
         prResize(); // resize if capacity >= 4 * size
     }
 }
-template <typename type> long long int myVector<type>::find(type x){
+template <typename type> long long int vector<type>::find(type x){
     for (int i = 0; i < size; i++){
         if (x == elements[i]){
             return i;
@@ -136,7 +133,7 @@ template <typename type> long long int myVector<type>::find(type x){
     }
     return -1;
 }
-template <typename type> long long int myVector<type>::find(type x, long long int st, long long int fin){
+template <typename type> long long int vector<type>::find(type x, long long int st, long long int fin){
     for (int i = st; i < fin; i++){
         if (x == elements[i]){
             return i;
@@ -144,7 +141,7 @@ template <typename type> long long int myVector<type>::find(type x, long long in
     }
     return -1;
 }
-template <typename type> long long int myVector<type>::count(type x){
+template <typename type> long long int vector<type>::count(type x){
     long long int count = 0;
     for (int i = 0; i < size; i++){
         if (elements[i] == x){
@@ -153,7 +150,7 @@ template <typename type> long long int myVector<type>::count(type x){
     }
     return count;
 }
-template <typename type> long long int myVector<type>::count(type x, long long int st, long long int fin){
+template <typename type> long long int vector<type>::count(type x, long long int st, long long int fin){
     long long int count = 0;
     for (int i = st; i < fin; i++){
         if (elements[i] == x){
